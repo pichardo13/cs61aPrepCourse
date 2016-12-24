489a7ef3a3d432b4c01eeac5a6d4ae9fd46a8139
pichardo13 <kpichardo13@berkeley.edu>
2016-12-23 23:04:08 -0800

    Week 1 hw

---
+++ chapter2/chapter2.rb
@@ -0,0 +1,13 @@
+puts 1 + 2
+
+#Hours in a year#
+puts 360 * 24 
+
+#Minutes in a decade#
+puts 60 * 24 * 365.25 * 10
+
+#Age in seconds#
+puts 18 * 365 * 24 * 60 * 60
+
+#our dear author's age#
+puts 1160000000 /(365 * 24 * 60 * 60)
