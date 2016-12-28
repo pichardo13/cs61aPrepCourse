456fb3ea2d9f3611231ed3314eea7d8c5fc055d1
pichardo13 <kpichardo13@berkeley.edu>
2016-12-27 21:03:51 -0800

    Update FullNameGreeding.rb

--- chapter5/FullNameGreeding.rb
+++ chapter5/FullNameGreeding.rb
@@ -5,9 +5,3 @@ mname = gets.chomp
 puts 'What is your last name?'
 lname = gets.chomp
 puts "Please to meet you " + namee + " " + mname + " "+ lname + "!"
-
-
-puts "what is your favorite number?"
-number = gets.chomp
-new_number = number.to_i + 1
-puts "Your new number should be " + new_number.to_s
