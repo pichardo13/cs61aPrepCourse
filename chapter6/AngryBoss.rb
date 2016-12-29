4b0a019fcb4f75b4747e6f237c82a68ae2595a3e
pichardo13 <kpichardo13@berkeley.edu>
2016-12-29 00:11:42 -0800

    week 2

---
+++ chapter6/AngryBoss.rb
@@ -0,0 +1,3 @@
+puts "WHAT DO YOU WANT?"
+answer = gets.chomp
+puts "WHAT DO YOU MEAN YOU WANT " + answer.upcase + "? YOU'RE FIRED!"
---
+++ chapter6/TableofContents.rb
@@ -0,0 +1,7 @@
+
+line_width = 50
+puts('Table of Contents'.center(line_width))
+puts('Chapter 1: Getting Started'.ljust(line_width/3) + 'Page'.rjust(line_width/3) + '1'.rjust(line_width/3))
+puts('Chapter 2: Numbers'.ljust(line_width/3) + 'Page'.rjust(line_width/3) +'3'.rjust(line_width/3))
+puts('Chapter 3: Letters'.ljust(line_width/3) + 'Page'.rjust(line_width/3) + '13'.rjust(line_width/3))
+
