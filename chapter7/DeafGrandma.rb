f8bfafd1772740a587e338150f11f191beb3785d
pichardo13 <kpichardo13@berkeley.edu>
2017-01-06 14:36:27 -0800

    updated

--- chapter7/DeafGrandma.rb
+++ chapter7/DeafGrandma.rb
@@ -1,7 +1,8 @@
 while true
 answer = gets.chomp
 	if answer == answer.upcase
-		puts "NO, NOT SINCE 1938!"
+		year = rand(1938...1960)
+		puts "NO, NOT SINCE " + year.to_s + "!"
 		break
 	else	
 		puts "HUH?! SPEAK UP, SONNY!"
